b8c8123704d8e247f4e6be92041ae204d6ff2036
CJY <cjy3458@naver.com>
2023-08-05 15:49:13 +0900

    ..

--- 5ing/src/App.js
+++ 5ing/src/App.js
@@ -1,16 +1,17 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
-import Randing from "./pages/randing/Randing";
 
-const App = () => {
+import LandingPage from "./pages/LandingPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Randing />}></Route>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
