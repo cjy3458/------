535c82e73d6f49222dd64fff2d5a336bbbb55aa9
CJY <cjy3458@naver.com>
2023-08-11 17:01:37 +0900

    [FEAT] 후기작성 페이지 구현

--- 5ing/src/App.js
+++ 5ing/src/App.js
@@ -10,12 +10,14 @@ import MentorInfoModal from "./components/community/mentor/MentorInfoModal";
 import TipsModal from "./components/community/tips/TipsModal";
 import ViewPostPage from "./pages/ViewPostPage";
 import ApplyPage from "./pages/ApplyPage";
+import ApplyAlertPage from "./pages/ApplyAlertPage";
+import ReviewPage from "./pages/ReviewPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/25" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/mentorsignup" element={<MentorSignup />} />
@@ -30,6 +32,8 @@ function App() {
         </Route>
         <Route path="/viewPost/:number" element={<ViewPostPage />} />
         <Route path="/apply" element={<ApplyPage />} />
+        <Route path="/5321" element={<ApplyAlertPage />} />
+        <Route path="/" element={<ReviewPage />} />
       </Routes>
     </BrowserRouter>
   );
